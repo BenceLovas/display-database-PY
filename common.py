3bef82bb2a560dda852fa46c2db2bea76058a8f5
Bence Lovas <bansoul1@gmail.com>
2017-08-24 19:35:43 +0200

    print menu

--- common.py
+++ common.py
@@ -3,16 +3,16 @@ import psycopg2
 CONNECTION_STR = "dbname='bans' user='bans' host='localhost' password='pass'"
 
 
-def query(query_str, connection_str=CONNECTION_STR):
+def query(sql_str, connection_str=CONNECTION_STR):
     try:
         with psycopg2.connect(connection_str) as connection:
             with connection.cursor() as cursor:
-                if query_str.startswith("SELECT"):
-                    cursor.execute(query_str)
+                if sql_str.startswith("SELECT"):
+                    cursor.execute(sql_str)
                     return cursor.fetchall()
                 else:
-                    cursor.execute(query_str)
+                    cursor.execute(sql_str)
                     connection.commit()
-                    return query_str.split()[0] + " done."
+                    return sql_str.split()[0] + " done."
     except psycopg2.DatabaseError as exception:
         print(exception)
